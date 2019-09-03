ae0853962729f7430735d4427ca61e14d6ac888d
Dan Smith <daniel.smith8795@gmail.com>
2019-09-03 14:42:44 -0500

    Fixed routing for home and survey

--- FriendFInder/server.js
+++ FriendFInder/server.js
@@ -7,8 +7,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "view.html"));
+    // Path to home page
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+
+});
+
+app.get("/survey", function (req, res) {
+    //Path to survey
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
 
