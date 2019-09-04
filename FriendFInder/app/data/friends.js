c030670bd9fa533a1ab2d859a74282807abb5d0f
Dan Smith <daniel.smith8795@gmail.com>
2019-09-04 13:51:58 -0500

    adding json data to friends.js and more content to HTML

---
+++ FriendFInder/app/data/friends.js
@@ -0,0 +1,170 @@
+var friends = [
+    {
+        name: "Dwight Schrute",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Jim Halpert",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Pam Beesly",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Michael Scott",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Ryan Howard",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Toby Flenderson",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Stanley Hudson",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Kevin Malone",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Meredith Palmer",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Angela Martin",
+        photo: "ipsum.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
+
+/*
+Questionaire: on a scale of 1 to 5 how strongly do you agree with the following statements?
+1 I have a strong sense of humor and love to joke around
+2 I have a compulsive need to have everyone love me
+3 I think Hilary Swank is hot
+4
+*/
