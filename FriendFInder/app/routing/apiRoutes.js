3e4aae161ce62642c57c8a10cea704091bcdb1ae
Dan Smith <daniel.smith8795@gmail.com>
2019-09-08 21:59:58 -0500

    Working on matching people

--- FriendFInder/app/routing/apiRoutes.js
+++ FriendFInder/app/routing/apiRoutes.js
@@ -6,9 +6,16 @@ module.exports = function (app) {
     })
 
     app.post("/api/friends", function (req, res) {
-        var newFriend = req.body
-        console.log(newFriend)
-        friends.push(newFriend)
+        var newFriend = req.body;
+        console.log(newFriend);
+        friends.push(newFriend);
+        var scores = newFriend.scores;
+        var sum = 0;
+        for (var i = 0; i < 10; i++) {
+            sum += parseInt(scores[i])
+        }
+        console.log("Total Score =" + sum)
+        //added in score calc to find new user score, need to compare to existing scores and find match
     })
 }
 
